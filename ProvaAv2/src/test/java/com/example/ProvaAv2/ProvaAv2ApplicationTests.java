d1cf180f2c0f26c2362ce6a398b34b4551ad0d24
Gustavo Jorge <99773984+GustavoJorgge@users.noreply.github.com>
2024-06-20 20:25:46 -0300

    subindo projeto

---
+++ ProvaAv2/src/test/java/com/example/ProvaAv2/ProvaAv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProvaAv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaAv2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
